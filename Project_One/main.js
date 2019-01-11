9f1afc566b6d8a683bfb015f537bd38f28b26c40
Cristin Ionut Ghihanis <ionut.ghihanis@gmail.com>
2019-01-11 23:39:15 +0200

    de acasa

--- Project_One/main.js
+++ Project_One/main.js
@@ -1,4 +1,4 @@
-// sursa de date pe care o primim impreuna cu imaginile din directorul img
+
 const images = [
     { "src": "./img/img1.jpg", "alt": "1 Nature" },
     { "src": "./img/img2.jpg", "alt": "2 Fjords" }, 
@@ -6,38 +6,68 @@ const images = [
     { "src": "./img/img4.jpg", "alt": "4 Lights" }
 ];
 
+//Creating variables, index = the current picture, bInainte, bInapoi = left & right buttons, 
+//bilute = generating the dots menu
+//biluta = tracks which dot is hovered
+
 var bilute = document.querySelector(".meniu");
 var bInainte = document.getElementById('inainte');
 var bInapoi = document.getElementById('inapoi');
 var index = 0;
 
+//This is the mai funtion allowing you to browse & view pictures
 function showImage(image){
-    document.getElementById('slide').src = images[image].src;
-    document.getElementById('slide').alt = images[image].alt;
-    if (index === 0)
-        bInapoi.style.display = 'none'
-    else if (index === images.length-1)
-        bInainte.style.display = 'none'
+    if (index < 0){//if index drops below 0, it'll reset to its max value
+        document.getElementById('slide').src = images[images.length - 1].src;
+        document.getElementById('slide').alt = images[images.length - 1].alt;
+        index = images.length - 1;  
+    }   
+    else if (index === images.length){//if index exceeds max value, it'll reset to 0
+        document.getElementById('slide').src = images[0].src;
+        document.getElementById('slide').alt = images[0].alt;
+        index = 0;
+    }
+    else{
+        document.getElementById('slide').src = images[image].src;
+        document.getElementById('slide').alt = images[image].alt;
+    }
+    
 }
-function createBiluta(tag,bTitle,bClass){
-    var t2 = document.createElement(tag);
-    t2.className = bClass;
-    t2.title = bTitle;
-    bilute.appendChild(t2);
+//This function creates the dots below the picture
+function createBiluta(param){
+    bilute.innerHTML = ''; //reset the menu element
+    for (var i=0; i<images.length; i++){
+        //creating dots (bilute)
+        var biluta = document.createElement('span');
+        biluta.className = 'biluta';
+        biluta.title = images[i].alt;
+        bilute.appendChild(biluta);
+        //making the dot active when its coresponding image is displayed
+        if (i === param)
+            biluta.classList.add('activ');
+        //adding click events for dots
+        biluta.addEventListener('click', () => {
+            console.log(i,index);
+            createBiluta(i);
+            showImage(i);
+            console.log("He clicked biluta number:"+i);
+        });        
+    }
+    index = param;
 }
 
-for (var i=0; i<images.length; i++){
-    createBiluta('span', images[i].alt, 'biluta'+i); 
-}
-
-window.addEventListener("load", ()=> showImage(index));
+window.addEventListener("load", ()=> {
+    showImage(index);
+    createBiluta(index);
+});
 
 bInainte.addEventListener("click", ()=> {
-    if (index < images.length - 1)
-        showImage(++index);
+    showImage(++index);
+    createBiluta(index);
  } );
 
 bInapoi.addEventListener("click", ()=> {
-    if (index > 0) 
-        showImage(--index);
+    showImage(--index);
+    createBiluta(index);
  } );
+
